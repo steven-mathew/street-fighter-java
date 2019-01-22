123e9b32998ca5a6e93b6530fe734168b357cb23
Steven Mathew <40929644+indra963@users.noreply.github.com>
2019-01-22 00:15:39 -0500

    Update Creature.java

--- src/com/streetFighter/entities/Creature.java
+++ src/com/streetFighter/entities/Creature.java
@@ -1,7 +1,6 @@
 package com.streetFighter.entities;
 
 public abstract class Creature extends Entity {
-	
 	// every subclass of creature has an x,y
 	public Creature(float x, float y) {
 		super(x, y);
